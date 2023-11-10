ea9f9f7d4f1e9d6fe5f46d9a19c28510e5f44269
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-10 22:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const form = document.querySelector("form");
+console.log(form);
+//
+form.addEventListener("submit" , onSubmit);
+//
+function onSubmit(event){
+    console.log("submited");
+    //prevets default behaviours
+    event.preventDefault();
+    // creating an object for storing values;
+    let employee = {
+        First Name:form.FirstName.value,
+        Last Name:form.LastName.value,
+        Phone Number:form.PhoneNumber.value,
+        Email ID:form.EmailID.value
+
+
+    }
+    //I will store all the values into in a string
+
+    let alertString ="";
+
+    for(let prop in employee)
+       {
+        alertString=alertString+`${prop}:${employee[prop]}\n`;
+       }
+    
+    
+    //
+    alert(alertString);
+
+
+    
+}
